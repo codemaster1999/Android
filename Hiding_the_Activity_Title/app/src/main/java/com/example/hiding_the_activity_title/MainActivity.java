45cea0411a33647868ec79569934f549c0583088
codemaster1999 <59681015+codemaster1999@users.noreply.github.com>
2020-01-09 12:43:16 -0800

    Create MainActivity.java

---
+++ Hiding_the_Activity_Title/app/src/main/java/com/example/hiding_the_activity_title/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hiding_the_activity_title;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+}
